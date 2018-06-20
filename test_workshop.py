d504ce9ae99815210d85600eb42e3dd50018c2dc
Christopher Woods <chryswoods@gmail.com>
2018-06-20 22:18:17 +0100

    Fixed typos

--- test_workshop.py
+++ test_workshop.py
@@ -4,65 +4,65 @@ import os
 
 nfailed = 0
 
-sys.write("Tesing git... ")
+sys.stdout.write("Tesing git... ")
 
 if os.system("git help > /dev/null 2>&1") == 0:
-    sys.write(" PASS\n")
+    sys.stdout.write(" PASS\n")
 else:
-    sys.write(" FAIL\n")
+    sys.stdout.write(" FAIL\n")
     nfailed += 1
 
-sys.write("Testing jupyter... ")
+sys.stdout.write("Testing jupyter... ")
 
 try:
     import jupyter
-    sys.write(" PASS\n")
+    sys.stdout.write(" PASS\n")
 except:
-    sys.write(" FAIL\n")
+    sys.stdout.write(" FAIL\n")
     nfailed += 1
 
-sys.write("Testing docker... ")
+sys.stdout.write("Testing docker... ")
 
 if os.system("docker images > /dev/null 2>&1") == 0:
-    sys.write(" PASS\n")
+    sys.stdout.write(" PASS\n")
 else:
-    sys.write(" FAIL\n")
+    sys.stdout.write(" FAIL\n")
     nfailed += 1
 
-sys.write("Testing az... ")
+sys.stdout.write("Testing az... ")
 
 if os.system("az help > /dev/null 2>&1") == 0:
-    sys.write(" PASS\n")
+    sys.stdout.write(" PASS\n")
 else:
-    sys.write(" FAIL\n")
+    sys.stdout.write(" FAIL\n")
     nfailed += 1
 
-sys.write("Testing kubectl... ")
+sys.stdout.write("Testing kubectl... ")
 
 if os.system("kubectl help > /dev/null 2>&1") == 0:
-    sys.write(" PASS\n")
+    sys.stdout.write(" PASS\n")
 else:
-    sys.write(" FAIL\n")
+    sys.stdout.write(" FAIL\n")
     nfailed += 1
 
-sys.write("Testing kubectl... ")
+sys.stdout.write("Testing kubectl... ")
  
 if os.system("kubectl help > /dev/null 2>&1") == 0:
-    sys.write(" PASS\n")
+    sys.stdout.write(" PASS\n")
 else:
-    sys.write(" FAIL\n")
+    sys.stdout.write(" FAIL\n")
     nfailed += 1
 
-sys.write("Testing helm... ")
+sys.stdout.write("Testing helm... ")
 
 if os.system("helm help > /dev/null 2>&1") == 0:
-    sys.write(" PASS\n")
+    sys.stdout.write(" PASS\n")
 else:
-    sys.write(" FAIL\n")
+    sys.stdout.write(" FAIL\n")
     nfailed += 1
 
 if nfailed > 0:
-    sys.write("\nThere have been some failures - please review.\n")
-    sys.write("Email chryswoods@gmail.com if you have any questions.\n")
+    sys.stdout.write("\nThere have been some failures - please review.\n")
+    sys.stdout.write("Email chryswoods@gmail.com if you have any questions.\n")
 else:
-    sys.write("Success. Looks like everything has installed correctly :-)\n")
+    sys.stdout.write("Success. Looks like everything has installed correctly :-)\n")
